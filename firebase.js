1d64799d127bf869034f97832b18b76b927d6756
Eddy980814 <Wag123890@gmail.com>
2025-04-09 22:47:18 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Your Firebase config here
+const firebaseConfig = {
+  apiKey: "AIzaSyBjA9Oln59AiplMoOMrW_tzNLfV8RFRojQ",
+  authDomain: "nrpay-abbce.firebaseapp.com",
+  projectId: "nrpay-abbce",
+  storageBucket: "nrpay-abbce.firebasestorage.app",
+  messagingSenderId: "342356133578",
+  appId: "1:342356133578:web:86e13f16ae0a40be15d633"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
